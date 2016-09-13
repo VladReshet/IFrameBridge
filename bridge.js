dd208dc7c2a2277e6ada90f01c4708e1e712ce31
VLReshet <vlreshet@gmail.com>
2016-09-13 11:39:44 +0300

    Create bridge.js

---
+++ bridge.js
@@ -0,0 +1,43 @@
+// bridge for cross-domains messages between iframe and parent page
+var IFrameBridge = function(send){
+    var routes = {};
+
+    var sender = function(route, data){
+        send(JSON.stringify({
+            route : route,
+            data : data
+        }));
+    };
+
+    var handleRoute = function(event){
+        var data = JSON.parse(event.data);
+
+        var route = data.route;
+        var data = data.data;
+
+        if(routes[route] !== undefined){
+            console.info('Route ' + route + ' was called');
+            routes[route](data, sender);
+        }else{
+            console.error('Route ' + route + ' is undefined');
+        }
+    };
+
+    return {
+        init : function(){
+            window.addEventListener('message', function(event){
+                handleRoute(event);
+            });
+        },
+
+        send : sender,
+
+        addRoute : function(route, callback){
+            routes[route] = callback;
+        },
+
+        getRoutes : function(){
+            return routes;
+        }
+    };
+};
